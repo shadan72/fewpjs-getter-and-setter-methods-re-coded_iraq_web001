493ca4499d9dd4e884c4df09aec7ed2fe68d2c74
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-16 18:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius=radius
+      pi=Math.PI()
+  }
+get diameter(){
+  return 2 * this.radius
+}
+get circumference(){
+  
+}
+get area(){
+  
+}
+}
